179ae160f1cc4cd116741a8f2caa8d352ab4a1de
Daniel Stutzman <dtstutz@gmail.com>
2014-01-09 11:41:30 -0700

    Install nginx with custom nginx.conf.

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,3 +5,19 @@ user node['online-ruby-tutor']['user'] do
   system true
   shell '/bin/bash'
 end
+
+include_recipe 'apt'
+
+apt_package 'nginx' do
+  action :install
+end
+
+cookbook_file '/etc/nginx/nginx.conf' do
+  source 'nginx.conf'
+  action :create # will replace the file
+  notifies :restart, 'service[nginx]', :delayed # will start if not started
+end
+
+service 'nginx' do
+  action :start
+end
