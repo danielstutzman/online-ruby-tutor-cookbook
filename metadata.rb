9fd56aa2c291fb695263fdc7ba47d48d98d6ce5e
Daniel Stutzman <dtstutz@gmail.com>
2014-01-09 10:43:12 -0700

    Started cookbook with berks cookbook online-ruby-tutor-cookbook.

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'online-ruby-tutor-cookbook'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures online-ruby-tutor-cookbook'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
