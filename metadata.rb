900bfaef9b3e53f148dfefa633755fdf8b24860e
Daniel Stutzman <dtstutz@gmail.com>
2014-01-09 11:05:44 -0700

    Fill out some blanks.

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,7 @@
 name             'online-ruby-tutor-cookbook'
-maintainer       'YOUR_NAME'
-maintainer_email 'YOUR_EMAIL'
+maintainer       'Daniel Stutzman'
+maintainer_email 'dtstutz@gmail.com'
 license          'All rights reserved'
-description      'Installs/Configures online-ruby-tutor-cookbook'
+description      'Installs/Configures online-ruby-tutor'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
-
